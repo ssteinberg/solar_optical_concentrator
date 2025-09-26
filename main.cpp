c15792798a7782ea354e99f8eb4b6d95eba5aadd
Benjamin Colussi <bcolussi@gmail.com>
2025-09-25 20:11:33 -0400

    hi

--- main.cpp
+++ main.cpp
@@ -35,8 +35,10 @@ constexpr float_type EPSILON = 0.01;
 constexpr float_type EPSILON_OVER_TWO = EPSILON / 2;
 
 // choose what to build
-constexpr bool FLAT_TARGET = true;
-constexpr bool CYLINDRICAL_TARGET = true;
+constexpr bool INFINITE_SYSTEM = false;
+constexpr bool FINITE_SYSTEM = true;
+constexpr bool FLAT_TARGET = false;
+constexpr bool CYLINDRICAL_TARGET = false;
 constexpr bool PARABOLIC_MIRROR = false;
 constexpr bool SCALE_INVARIANT = false;
 
@@ -109,20 +111,20 @@ struct TwoMirrorConcentrator {
     // fields
     Mirror M1, M2;
 
-    // compute the line segments of the two mirrors
-    void build(const TargetShape& targetShape, const bool& inverted, const float_type& L, const float_type& f, const float_vec& K_in, const float_type& dB, const float_type& B_max) {
+    // build infinite system
+    void buildInfinite(const TargetShape& ts, const bool& inv, const float_type& L, const float_type& f, const float_vec& K_in, const float_type& dB, const float_type& B_max) {
 
         // incoming intensity
         const auto& I = [=](const float_type alpha) {
-            if (targetShape == TargetShape::FLAT) return std::cos(alpha);
-            else if (targetShape == TargetShape::CYLINDRICAL) return float_type(1);
+            if (ts == TargetShape::FLAT) return std::cos(alpha);
+            else if (ts == TargetShape::CYLINDRICAL) return float_type(1);
             else return float_type(0);
         };
 
         // outgoing intensity
         const auto& S = [=](const float_type beta) {
-            if (targetShape == TargetShape::FLAT) return std::cos(beta);
-            else if (targetShape == TargetShape::CYLINDRICAL) return float_type(1);
+            if (ts == TargetShape::FLAT) return std::cos(beta);
+            else if (ts == TargetShape::CYLINDRICAL) return float_type(1);
             else return float_type(0);
         };
 
@@ -144,7 +146,7 @@ struct TwoMirrorConcentrator {
 
             // (7)
             const float_type I = 1; // depends on I = const.
-            const float_type dy = (inverted ? -1 : 1) * S(B) / I * dB;
+            const float_type dy = (inv ? -1 : 1) * S(B) / I * dB;
             const float_type dx = dy * (p.y - r * sinB) / (r * (cosB - 1) + 2 * (L + f));
             const float_vec p_new(p.x + dx, p.y + dy);
 
@@ -174,6 +176,61 @@ struct TwoMirrorConcentrator {
             R = R_new;
             r = r_new;
             B = B_new;
+        }
+    }
+
+    // build finite system
+    void buildFinite(const TargetShape& ts, const float_type& f1, const float_type& L, const float_type& f2, const float_type& da, const float_type& a_max) {
+
+        // initial conditions
+        float_type a(0), B(0), r1(f1), r2(f2);
+        float_vec p1(-f1 - L, 0), p2(f2, 0), n1(1, 0), n2(-1, 0);
+        const float_type F(2 * f1 + L + 2 * f2);
+        const float_vec source(-L, 0), target(0, 0);
+
+        // numerical integration
+        while (a < a_max) {
+
+            // precomputation
+            const float_type cosa(std::cos(a)), sina(std::sin(a));
+            const float_type cosB(std::cos(B)), sinB(std::sin(B));
+            const float_type cosaB(std::cos(a + B)), sinaB(std::sin(a + B));
+
+            // M1
+            const float_type a_new = a + da;
+            const float_type dr1 = (r1 * r2 * sinaB + L * r1 * sina) / (r2 * cosaB + L * cosa - r2 + F) * da;
+            const float_type r1_new = r1 + dr1;
+            const float_vec p1_new(-r1_new * std::cos(a_new) - L, r1_new * std::sin(a_new));
+
+            // M2
+            const float_type dB = da;
+            const float_type B_new = B + dB;
+            const float_type dr2 = (r1 * r2 * sinaB + L * r2 * sinB) / (r1 * cosaB + L * cosB - r1 + F) * dB;
+            const float_type r2_new = r2 + dr2;
+            const float_vec p2_new(r2_new * std::cos(B_new), r2_new * std::sin(B_new));
+
+            // ray directions
+            const float_vec v1((p1_new - source) / r1_new);
+            const float_vec u((p2_new - p1_new) / (F - r1_new - r2_new));
+            const float_vec v2((target - p2_new) / r2_new);
+
+            // normals
+            const float_vec n1_new(normalize(u - v1));
+            const float_vec n2_new(normalize(v2 - u));
+
+            // store coordinates and normals
+            M1.addSegment(p1, p1_new, n1, n1_new);
+            M2.addSegment(p2, p2_new, n2, n2_new);
+
+            // update
+            a = a_new;
+            r1 = r1_new;
+            p1 = p1_new;
+            B = B_new;
+            r2 = r2_new;
+            p2 = p2_new;
+            n1 = n1_new;
+            n2 = n2_new;
         }
     }
 
@@ -252,27 +309,50 @@ struct ParabolicMirror {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// build two-mirror concentrator
-TwoMirrorConcentrator build2MC(const TargetShape& ts, const bool& inv, const float_type& L, const float_type& f, const float_vec& K_in, const float_type& dB, const float_type& B_max,
+// build infinite two-mirror concentrator
+TwoMirrorConcentrator buildInf2MC(const TargetShape& ts, const bool& inv, const float_type& L, const float_type& f, const float_vec& K_in, const float_type& dB, const float_type& B_max,
     const std::string& fileName) {
 
     // build
-    TwoMirrorConcentrator TMC;
-    TMC.build(ts, inv, L, f, K_in, dB, B_max);
+    TwoMirrorConcentrator inf2MC;
+    inf2MC.buildInfinite(ts, inv, L, f, K_in, dB, B_max);
 
     // write
     std::ofstream file;
     file.open(fileName);
-    for (int i = 0, n = static_cast<int>(TMC.M1.segments.size()); i < n; ++i) {
-        file << TMC.M1.segments[i].first.x << "," << TMC.M1.segments[i].first.y << ",";
-        file << TMC.M2.segments[i].first.x << "," << TMC.M2.segments[i].first.y << "\n";
+    for (int i = 0, n = static_cast<int>(inf2MC.M1.segments.size()); i < n; ++i) {
+        file << inf2MC.M1.segments[i].first.x << "," << inf2MC.M1.segments[i].first.y << ",";
+        file << inf2MC.M2.segments[i].first.x << "," << inf2MC.M2.segments[i].first.y << "\n";
     }
-    file << TMC.M1.segments.back().second.x << "," << TMC.M1.segments.back().second.y << ",";
-    file << TMC.M2.segments.back().second.x << "," << TMC.M2.segments.back().second.y << "\n";
+    file << inf2MC.M1.segments.back().second.x << "," << inf2MC.M1.segments.back().second.y << ",";
+    file << inf2MC.M2.segments.back().second.x << "," << inf2MC.M2.segments.back().second.y << "\n";
     file.close();
 
     // return
-    return TMC;
+    return inf2MC;
+}
+
+// build finite two-mirror concentrator
+TwoMirrorConcentrator buildFin2MC(const TargetShape& ts, const float_type& f1, const float_type& L, const float_type& f2, const float_type& da, const float_type& a_max,
+    const std::string& fileName) {
+    
+    // build
+    TwoMirrorConcentrator fin2MC;
+    fin2MC.buildFinite(ts, f1, L, f2, da, a_max);
+
+    // write
+    std::ofstream file;
+    file.open(fileName);
+    for (int i = 0, n = static_cast<int>(fin2MC.M1.segments.size()); i < n; ++i) {
+        file << fin2MC.M1.segments[i].first.x << "," << fin2MC.M1.segments[i].first.y << ",";
+        file << fin2MC.M2.segments[i].first.x << "," << fin2MC.M2.segments[i].first.y << "\n";
+    }
+    file << fin2MC.M1.segments.back().second.x << "," << fin2MC.M1.segments.back().second.y << ",";
+    file << fin2MC.M2.segments.back().second.x << "," << fin2MC.M2.segments.back().second.y << "\n";
+    file.close();
+
+    // return
+    return fin2MC;
 }
 
 // build parabolic mirror
@@ -298,45 +378,95 @@ ParabolicMirror buildPM(const float_type& f, const float_type& dy, const float_t
 // main
 int main(int argc, char* argv[]) {
 
-    // input
-    const float_vec K_in(-1, 0);
-    const float_type dB(0.01);
+    // infinite system
+    if (INFINITE_SYSTEM) {
 
-    // flat target
-    TwoMirrorConcentrator TMCflat;
-    ParabolicMirror PMflat;
-    if (FLAT_TARGET) {
-        const TargetShape ts(TargetShape::FLAT);
-        const bool inv(false);
-        const float_type f(0.2);
-        const float_type L(8 * f);
-        const float_type B_max(80 * DEG_TO_RAD);
-        TMCflat = build2MC(ts, inv, L, f, K_in, dB, B_max, "data_2mc_flat.csv");
+        // input
+        const float_vec K_in(-1, 0);
+        const float_type dB(0.01);
 
-        // parabolic mirror
-        if (PARABOLIC_MIRROR) {
-            const float_type dy(dB);
-            PMflat = buildPM(f, dy, B_max, "data_pm_flat.csv");
+        // flat target
+        TwoMirrorConcentrator infFlat2MC;
+        ParabolicMirror infFlatPM;
+        if (FLAT_TARGET) {
+            const TargetShape ts(TargetShape::FLAT);
+            const bool inv(false);
+            const float_type f(0.2);
+            const float_type L(8 * f);
+            const float_type B_max(80 * DEG_TO_RAD);
+            infFlat2MC = buildInf2MC(ts, inv, L, f, K_in, dB, B_max, "data_2mc_inf_flat.csv");
+
+            // parabolic mirror
+            if (PARABOLIC_MIRROR) {
+                const float_type dy(dB);
+                infFlatPM = buildPM(f, dy, B_max, "data_pm_inf_flat.csv");
+            }
+        }
+
+        // cylindrical target
+        TwoMirrorConcentrator infCyl2MC;
+        ParabolicMirror infCylPM;
+        if (CYLINDRICAL_TARGET) {
+            const TargetShape ts(TargetShape::CYLINDRICAL);
+            const bool inv(true);
+            const float_type f(0.5);
+            const float_type L(6 * f);
+            const float_type B_max(150 * DEG_TO_RAD);
+            infCyl2MC = buildInf2MC(ts, inv, L, f, K_in, dB, B_max, "data_2mc_inf_cyl.csv");
+
+            // parabolic mirror
+            if (PARABOLIC_MIRROR) {
+                const float_type dy(dB);
+                infCylPM = buildPM(f, dy, B_max, "data_pm_inf_cyl.csv");
+            }
+        }
+
+        
+
+        // ray trace
+        const float_type h_inc = 0.01;
+        if (FLAT_TARGET) {
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> meanPaths;
+            infFlat2MC.traceRays(meanPaths, K_in, -infFlat2MC.M1.segments.back().second.y, h_inc, infFlat2MC.M1.segments.back().second.y, "data_2mc_flat_mean.csv");
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrPosPaths;
+            float_vec extrPosDir(-std::cos(EPSILON_OVER_TWO), std::sin(EPSILON_OVER_TWO));
+            infFlat2MC.traceRays(extrPosPaths, extrPosDir, -infFlat2MC.M1.segments.back().second.y, h_inc, infFlat2MC.M1.segments.back().second.y, "data_2mc_flat_extr_pos.csv");
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrNegPaths;
+            float_vec extrNegDir(-std::cos(EPSILON_OVER_TWO), -std::sin(EPSILON_OVER_TWO));
+            infFlat2MC.traceRays(extrNegPaths, extrNegDir, -infFlat2MC.M1.segments.back().second.y, h_inc, infFlat2MC.M1.segments.back().second.y, "data_2mc_flat_extr_neg.csv");
+        }
+        if (CYLINDRICAL_TARGET) {
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> meanPaths;
+            infCyl2MC.traceRays(meanPaths, K_in, infCyl2MC.M1.segments.back().second.y, h_inc, -infCyl2MC.M1.segments.back().second.y, "data_2mc_cyl_mean.csv");
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrPosPaths;
+            float_vec extrPosDir(-std::cos(EPSILON_OVER_TWO), std::sin(EPSILON_OVER_TWO));
+            infCyl2MC.traceRays(extrPosPaths, extrPosDir, infCyl2MC.M1.segments.back().second.y, h_inc, -infCyl2MC.M1.segments.back().second.y, "data_2mc_cyl_extr_pos.csv");
+            std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrNegPaths;
+            float_vec extrNegDir(-std::cos(EPSILON_OVER_TWO), -std::sin(EPSILON_OVER_TWO));
+            infCyl2MC.traceRays(extrNegPaths, extrNegDir, infCyl2MC.M1.segments.back().second.y, h_inc, -infCyl2MC.M1.segments.back().second.y, "data_2mc_cyl_extr_neg.csv");
         }
     }
 
-    // cylindrical target
-    TwoMirrorConcentrator TMCcyl;
-    ParabolicMirror PMcyl;
-    if (CYLINDRICAL_TARGET) {
-        const TargetShape ts(TargetShape::CYLINDRICAL);
-        const bool inv(true);
-        const float_type f(0.5);
-        const float_type L(6 * f);
-        const float_type B_max(150 * DEG_TO_RAD);
-        TMCcyl = build2MC(ts, inv, L, f, K_in, dB, B_max, "data_2mc_cyl.csv");
 
-        // parabolic mirror
-        if (PARABOLIC_MIRROR) {
-            const float_type dy(dB);
-            PMcyl = buildPM(f, dy, B_max, "data_pm_cyl.csv");
-        }
+
+    // finite system
+    if (FINITE_SYSTEM) {
+
+        const TargetShape ts = TargetShape::CYLINDRICAL;
+        const float_type f1(1), L(10), f2(1), da(0.0001), a_max(135 * DEG_TO_RAD);
+        TwoMirrorConcentrator finCyl2MC;
+        finCyl2MC = buildFin2MC(ts, f1, L, f2, da, a_max, "data_2mc_fin_cyl.csv");
+
+        ParabolicMirror finCylPM;
+        // gonna have to build a 2PM concentrator ...
+
     }
+
+
+
+
+
+    /*
 
     // scale-invariant
     if (SCALE_INVARIANT) {
@@ -346,35 +476,10 @@ int main(int argc, char* argv[]) {
         constexpr float_type f(scale * 0.5);
         constexpr float_type L(scale * 3);
         constexpr float_type B_max(150 * DEG_TO_RAD);
-        build2MC(ts, inv, L, f, K_in, dB, B_max, "data_scale_inv.csv");
+        buildInf2MC(ts, inv, L, f, K_in, dB, B_max, "data_scale_inv.csv");
     }
 
-
-
-    // ray trace
-    const float_type h_inc = 0.01;
-    if (FLAT_TARGET) {
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> meanPaths;
-        TMCflat.traceRays(meanPaths, K_in, -TMCflat.M1.segments.back().second.y, h_inc, TMCflat.M1.segments.back().second.y, "data_2mc_flat_mean.csv");
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrPosPaths;
-        float_vec extrPosDir(-std::cos(EPSILON_OVER_TWO), std::sin(EPSILON_OVER_TWO));
-        TMCflat.traceRays(extrPosPaths, extrPosDir, -TMCflat.M1.segments.back().second.y, h_inc, TMCflat.M1.segments.back().second.y, "data_2mc_flat_extr_pos.csv");
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrNegPaths;
-        float_vec extrNegDir(-std::cos(EPSILON_OVER_TWO), -std::sin(EPSILON_OVER_TWO));
-        TMCflat.traceRays(extrNegPaths, extrNegDir, -TMCflat.M1.segments.back().second.y, h_inc, TMCflat.M1.segments.back().second.y, "data_2mc_flat_extr_neg.csv");
-    }
-    if (CYLINDRICAL_TARGET) {
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> meanPaths;
-        TMCcyl.traceRays(meanPaths, K_in, TMCcyl.M1.segments.back().second.y, h_inc, -TMCcyl.M1.segments.back().second.y, "data_2mc_cyl_mean.csv");
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrPosPaths;
-        float_vec extrPosDir(-std::cos(EPSILON_OVER_TWO), std::sin(EPSILON_OVER_TWO));
-        TMCcyl.traceRays(extrPosPaths, extrPosDir, TMCcyl.M1.segments.back().second.y, h_inc, -TMCcyl.M1.segments.back().second.y, "data_2mc_cyl_extr_pos.csv");
-        std::vector<std::tuple<float_vec, float_vec, float_vec, float_vec>> extrNegPaths;
-        float_vec extrNegDir(-std::cos(EPSILON_OVER_TWO), -std::sin(EPSILON_OVER_TWO));
-        TMCcyl.traceRays(extrNegPaths, extrNegDir, TMCcyl.M1.segments.back().second.y, h_inc, -TMCcyl.M1.segments.back().second.y, "data_2mc_cyl_extr_neg.csv");
-    }
-
-
+    */
 
     /*
 
